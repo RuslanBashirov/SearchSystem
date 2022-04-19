6941610b4028b511fe7ea6a56a028d952566f386
ruslan <bashirov.1994@mail.ru>
2022-04-19 10:11:41 +0300

    rm all

--- distributed-search-cluster-node/src/main/java/networking/OnRequestCallback.java
+++
@@ -1,9 +0,0 @@
-package networking;
-
-import java.io.IOException;
-
-public interface OnRequestCallback {
-    byte[] handleRequest(byte[] requestPayload) throws IOException;
-
-    String getEndpoint();
-}
